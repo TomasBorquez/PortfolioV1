28917e29399fec692eab58174deaf1f08a391d87
Tomas <tomasborquez13@gmail.com>
2022-05-25 10:39:07 -0300

    removed typescript cause its too overkill xd

--- dist/components/test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let arr = [1, "Mosh"];
-arr.forEach(n => console.log(n));
-//# sourceMappingURL=test.js.map
--- src/components/test.ts
+++
@@ -1,2 +0,0 @@
-let arr: [number, string] = [1, "Mosh"]
-arr.forEach(n => console.log(n))
