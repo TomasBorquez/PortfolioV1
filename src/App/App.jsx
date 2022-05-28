8216df96707b2d2e9a1d54e8bcaec5c124298a79
Tomas <tomasborquez13@gmail.com>
2022-05-28 02:09:35 -0300

    trying to deploy

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,30 +1,31 @@
 import React, { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Nav from '../components/Nav/Nav.jsx'
-import About from '../components/About/About.jsx'
+import Nav from '../components/Nav/Nav.jsx';
+import About from '../components/About/About.jsx';
 import './App.css';
 function App() {
   return (
     <div className="App">
-
       <Routes>
-        <Route path='/' element={
-              <Fragment>
-                <Nav />
-                <About />
-              </Fragment>
-            }>
-        </Route>
+        <Route
+          path="/PortfolioV1"
+          element={
+            <Fragment>
+              <Nav />
+              <About />
+            </Fragment>
+          }
+        ></Route>
 
-        <Route path='/works' element={
-              <Fragment>
-                <Nav />
-                <About />
-              </Fragment>
-        }>
-          
-        </Route>
-
+        <Route
+          path="/works"
+          element={
+            <Fragment>
+              <Nav />
+              <About />
+            </Fragment>
+          }
+        ></Route>
       </Routes>
       {/* <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/v551nqGeHhGn/github">GitHub</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a> */}
     </div>
